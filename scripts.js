1321d214d6cd0461a2a92c9fe3831a9e6361e3f9
Levente Simko <78898738+honeybadger-graphics@users.noreply.github.com>
2024-01-23 14:00:26 +0100

    V1.5
    Added other sites.

--- scripts.js
+++ scripts.js
@@ -26,3 +26,85 @@ let parody = document.getElementsByClassName("parody");
                 
 
             }
+
+            $(document).ready(function () {
+              $("#responseError1").hide();
+              document.getElementById("indokok").onchange = function() {
+                var x=document.getElementById("indokok");
+                var txt;
+                if( x.value=="error")
+                {  
+                  txt = "Mivel hibát észlelt a weblapunkon kérjük adja meg a dátumot mikor tapasztalta."
+                  document.getElementById("responseError2").innerHTML = txt;
+                  $("#responseError1").show();
+                }
+                else{
+                  txt ="";
+                  document.getElementById("responseError2").innerHTML = txt;
+                  $("#responseError1").hide();
+                }
+              }
+              $("#validate").click(function () {
+                  var nameValid= false;
+                  var emailValid= false;
+                  var messageValid= false;
+                  var reasonValid = false;
+                  var contactName=$("#contactName").val();
+                  var gdpr = document.getElementById("GDPR");
+                  var contactEmail=$("#contactEmail").val();
+                  var contactMessage=$("#contactMessage").val();
+                  var reason =$("#indokok").val();
+                  $("#contactName").css('border','1px solid #ccc');
+                  $("#contactEmail").css('border','1px solid #ccc');
+                  $("#contactMessage").css('border','1px solid #ccc');
+                  $("#indokok").css('border','1px solid #ccc');
+                  if(!gdpr.checked){
+                    alert("Fogadja el a a Adatkezeést!");
+                    return;
+                }
+                  if(contactName==""){
+                      alert("Név megadása kötelezö.");
+                      $("#contactName").css('border','2px solid #ff0000');
+                      return;
+                  }
+                  else{
+                    nameValid= true;
+                  }
+                  if(reason==""){
+                    alert("Adjon meg indokot.");
+                    $("#indokok").css('border','2px solid #ff0000');
+                    return;
+                  }
+                  else{
+                    reasonValid=true;
+                  }
+                  if(contactEmail==""){
+                      alert("Kapcsolatba lépési emailcím megadása kötelező.");
+                      $("#contactEmail").css('border','2px solid #ff0000');
+                      return;
+                  }
+                  else{
+                    emailValid=true;
+                  }
+                  if(contactMessage==""){
+                      alert("Nincs üzenet.");
+                      $("#contactMessage").css('border','2px solid #ff0000');
+                      return;
+                  }
+                  else{
+                    messageValid=true;
+                  }
+                  if(nameValid && reasonValid && emailValid && messageValid && gdpr.checked) {
+                   
+                      var txt;
+                      if (confirm("Biztos el akarja küldeni?")) {
+                          txt = "Elküldtük az üzenetét.";
+                      } else {
+                          txt = "Semmi nem történt.";
+                      }
+                      document.getElementById("response").innerHTML = txt;
+                  }
+                 
+              });
+             
+          });
